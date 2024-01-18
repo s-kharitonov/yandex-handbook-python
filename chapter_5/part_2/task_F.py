b40c64a630767e5f2508c8aadce4dec315f9cb20
skharitonov <skharitonov.k@gmail.com>
2024-01-18 20:51:20 +0300

    add task F solution

---
+++ chapter_5/part_2/task_F.py
@@ -0,0 +1,87 @@
+# Дроби v0.3
+# Продолжим разработку класса Fraction, который реализует предлагаемые дроби.
+#
+# Реализуйте бинарные операторы:
+#
+# + — сложение дробей, создаёт новую дробь;
+# - — вычитание дробей, создаёт новую дробь;
+# += — сложение дробей, изменяет дробь, переданную слева;
+# -= — вычитание дробей, изменяет дробь, переданную слева.
+# Примечание
+# Будем считать, что пользователь знает о запрете деления на ноль.
+# Все поля и методы, не требуемые в задаче,
+# следует инкапсулировать (называть с использованием ведущих символов нижнего подчёркивания).
+#
+# Ваше решение должно содержать только классы и функции.
+# В решении не должно быть вызовов инициализации требуемых классов.
+
+class Fraction:
+
+    def __init__(self, *args):
+        if len(args) == 1:
+            self.n, self.d = map(int, args[0].split('/'))
+        else:
+            self.n, self.d = args
+        self.__simplify()
+
+    def __simplify(self):
+        numerator, denominator = self.n, self.d
+        while denominator:
+            numerator, denominator = denominator, numerator % denominator
+        self.n, self.d = self.n // numerator, self.d // numerator
+
+    def __add__(self, other):
+        return Fraction(self.n * other.d + other.n * self.d, self.d * other.d)
+
+    def __iadd__(self, other):
+        self.n, self.d = self.n * other.d + other.n * self.d, self.d * other.d
+        self.__simplify()
+        return self
+
+    def __sub__(self, other):
+        return Fraction(self.n * other.d - other.n * self.d, self.d * other.d)
+
+    def __isub__(self, other):
+        self.n, self.d = self.n * other.d - other.n * self.d, self.d * other.d
+        self.__simplify()
+        return self
+
+    def numerator(self, number=0):
+        if not number:
+            return abs(self.n)
+        if self.n > 0:
+            self.n = abs(number)
+            self.__simplify()
+            if number < 0:
+                self.n = -self.n
+        elif self.n < 0:
+            self.n = abs(number)
+            self.__simplify()
+            if number > 0:
+                self.n = -self.n
+        return abs(self.n)
+
+    def denominator(self, number=0):
+        if not number:
+            return self.d
+        if self.n > 0:
+            self.d = abs(number)
+            self.__simplify()
+            if number < 0:
+                self.n = -self.n
+        elif self.n < 0:
+            self.n = abs(self.n)
+            self.d = abs(number)
+            self.__simplify()
+            if number > 0:
+                self.n = -self.n
+        return self.d
+
+    def __neg__(self):
+        return Fraction(-self.n, self.d)
+
+    def __str__(self):
+        return f'{self.n}/{self.d}'
+
+    def __repr__(self):
+        return f"Fraction('{self.n}/{self.d}')"
